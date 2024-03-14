3c1c732bfc65916efd843dd585acb184ef76bdd4
Willy Kloucek <34452982+wkloucek@users.noreply.github.com>
2024-03-14 15:01:40 +0100

    bump tooling (#500)

--- .bingo/Variables.mk
+++ .bingo/Variables.mk
@@ -17,32 +17,33 @@ GO     ?= $(shell which go)
 #	@echo "Running bingo"
 #	@$(BINGO) <flags/args..>
 #
-BINGO := $(GOBIN)/bingo-v0.8.1-0.20230820182247-0568407746a2
+BINGO := $(GOBIN)/bingo-v0.9.0
 $(BINGO): $(BINGO_DIR)/bingo.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/bingo-v0.8.1-0.20230820182247-0568407746a2"
-	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=bingo.mod -o=$(GOBIN)/bingo-v0.8.1-0.20230820182247-0568407746a2 "github.com/bwplotka/bingo"
+	@echo "(re)installing $(GOBIN)/bingo-v0.9.0"
+	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=bingo.mod -o=$(GOBIN)/bingo-v0.9.0 "github.com/bwplotka/bingo"
 
-GOMPLATE := $(GOBIN)/gomplate-v3.11.5
+GOMPLATE := $(GOBIN)/gomplate-v3.11.7
 $(GOMPLATE): $(BINGO_DIR)/gomplate.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/gomplate-v3.11.5"
-	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=gomplate.mod -o=$(GOBIN)/gomplate-v3.11.5 "github.com/hairyhenderson/gomplate/v3/cmd/gomplate"
+	@echo "(re)installing $(GOBIN)/gomplate-v3.11.7"
+	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=gomplate.mod -o=$(GOBIN)/gomplate-v3.11.7 "github.com/hairyhenderson/gomplate/v3/cmd/gomplate"
 
-HELM_DOCS := $(GOBIN)/helm-docs-v1.11.0
+HELM_DOCS := $(GOBIN)/helm-docs-v1.13.1
 $(HELM_DOCS): $(BINGO_DIR)/helm-docs.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/helm-docs-v1.11.0"
-	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=helm-docs.mod -o=$(GOBIN)/helm-docs-v1.11.0 "github.com/norwoodj/helm-docs/cmd/helm-docs"
+	@echo "(re)installing $(GOBIN)/helm-docs-v1.13.1"
+	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=helm-docs.mod -o=$(GOBIN)/helm-docs-v1.13.1 "github.com/norwoodj/helm-docs/cmd/helm-docs"
 
-KUBE_LINTER := $(GOBIN)/kube-linter-v0.6.4
+KUBE_LINTER := $(GOBIN)/kube-linter-v0.6.8
 $(KUBE_LINTER): $(BINGO_DIR)/kube-linter.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/kube-linter-v0.6.4"
-	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=kube-linter.mod -o=$(GOBIN)/kube-linter-v0.6.4 "golang.stackrox.io/kube-linter/cmd/kube-linter"
+	@echo "(re)installing $(GOBIN)/kube-linter-v0.6.8"
+	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=kube-linter.mod -o=$(GOBIN)/kube-linter-v0.6.8 "golang.stackrox.io/kube-linter/cmd/kube-linter"
 
-KUBECONFORM := $(GOBIN)/kubeconform-v0.6.3
+KUBECONFORM := $(GOBIN)/kubeconform-v0.6.4
 $(KUBECONFORM): $(BINGO_DIR)/kubeconform.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/kubeconform-v0.6.3"
-	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=kubeconform.mod -o=$(GOBIN)/kubeconform-v0.6.3 "github.com/yannh/kubeconform/cmd/kubeconform"
+	@echo "(re)installing $(GOBIN)/kubeconform-v0.6.4"
+	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=kubeconform.mod -o=$(GOBIN)/kubeconform-v0.6.4 "github.com/yannh/kubeconform/cmd/kubeconform"
+
