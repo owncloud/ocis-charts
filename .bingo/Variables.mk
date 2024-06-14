229b9b8bcb0b1aac3977642e43bdc76691e74021
Willy Kloucek <wkloucek@owncloud.com>
2024-06-14 13:54:43 +0200

    update tooling

--- .bingo/Variables.mk
+++ .bingo/Variables.mk
@@ -1,4 +1,4 @@
-# Auto generated binary variables helper managed by https://github.com/bwplotka/bingo v0.8. DO NOT EDIT.
+# Auto generated binary variables helper managed by https://github.com/bwplotka/bingo v0.9. DO NOT EDIT.
 # All tools are designed to be build inside $GOBIN.
 BINGO_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 GOPATH ?= $(shell go env GOPATH)
@@ -23,11 +23,11 @@ $(BINGO): $(BINGO_DIR)/bingo.mod
 	@echo "(re)installing $(GOBIN)/bingo-v0.9.0"
 	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=bingo.mod -o=$(GOBIN)/bingo-v0.9.0 "github.com/bwplotka/bingo"
 
-GOMPLATE := $(GOBIN)/gomplate-v3.11.7
+GOMPLATE := $(GOBIN)/gomplate-v3.11.8
 $(GOMPLATE): $(BINGO_DIR)/gomplate.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/gomplate-v3.11.7"
-	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=gomplate.mod -o=$(GOBIN)/gomplate-v3.11.7 "github.com/hairyhenderson/gomplate/v3/cmd/gomplate"
+	@echo "(re)installing $(GOBIN)/gomplate-v3.11.8"
+	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=gomplate.mod -o=$(GOBIN)/gomplate-v3.11.8 "github.com/hairyhenderson/gomplate/v3/cmd/gomplate"
 
 HELM_DOCS := $(GOBIN)/helm-docs-v1.13.1
 $(HELM_DOCS): $(BINGO_DIR)/helm-docs.mod
@@ -41,9 +41,9 @@ $(KUBE_LINTER): $(BINGO_DIR)/kube-linter.mod
 	@echo "(re)installing $(GOBIN)/kube-linter-v0.6.8"
 	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=kube-linter.mod -o=$(GOBIN)/kube-linter-v0.6.8 "golang.stackrox.io/kube-linter/cmd/kube-linter"
 
-KUBECONFORM := $(GOBIN)/kubeconform-v0.6.4
+KUBECONFORM := $(GOBIN)/kubeconform-v0.6.6
 $(KUBECONFORM): $(BINGO_DIR)/kubeconform.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/kubeconform-v0.6.4"
-	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=kubeconform.mod -o=$(GOBIN)/kubeconform-v0.6.4 "github.com/yannh/kubeconform/cmd/kubeconform"
+	@echo "(re)installing $(GOBIN)/kubeconform-v0.6.6"
+	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=kubeconform.mod -o=$(GOBIN)/kubeconform-v0.6.6 "github.com/yannh/kubeconform/cmd/kubeconform"
 
